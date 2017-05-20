46621d5bfea47dc8064a912290aa8901929f0f9c
Esteban <Esteban_lau_koo@hotmail.com>
2017-05-20 10:59:11 -0400

    initial look done

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+/**
+ * Created by esteb on 5/20/2017.
+ */
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var logger = require("morgan")
 
-var PORT = process.env.PORT || 4000
+var PORT = process.env.PORT || 3000
 
 var app = express();
 
@@ -12,7 +14,7 @@ var db = require("./models")
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
-app.use(logger("dev"))
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -27,9 +29,22 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+//require("./routes/html-routes.js")(app);
+//require("./routes/burger-api-routes.js")(app);
+//require("./routes/customer-api-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT)
-    });
+    })
+
+
+
+
+
+
+
+
+
+
 })
